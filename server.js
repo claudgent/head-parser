135fa5db2f1a69727ceea3a20d74cfe9f050ec12
Claudio Gentile <claudgent@gmail.com>
2017-02-08 22:01:09 +0000

    Built microservice header parser

---
+++ server.js
@@ -0,0 +1,28 @@
+
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Get request
+app.get('/', (req, res) => {
+  // get and parse OS info from user's get request header
+  const userAgent = req.get('user-agent');
+  const software = userAgent.match(/\(([^)]+)/).slice(1);
+
+  // get and parse language from user's get request header
+  const acceptLanguage = req.get('accept-language');
+  const lang = acceptLanguage.match(/^(.+),/).slice(-1);
+
+  // get host ip address from user's request header
+  const host = req.get('host');
+
+  // respond with json object containing header information
+  res.json({ ipAddress: host,
+    Software: software[0],
+    Language: lang[0],
+  });
+});
+
+// Start server
+app.listen(port);
